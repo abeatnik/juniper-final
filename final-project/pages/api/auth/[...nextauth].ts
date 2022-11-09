ab587c169aebae534f60e947ce9c817c63b0851a
Tabea Schneider <abeat@posteo.net>
2022-11-09 17:47:04 +0100

    start with GitHub Login via Next Auth [updat prisma schema and start with auth route]

---
+++ final-project/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+//boilerplate from: https://vercel.com/guides/nextjs-prisma-postgres
+
+import * as dotenv from "dotenv";
+dotenv.config();
+import { NextApiHandler } from "next";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import prisma from "../../../lib/prisma";
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+const options = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID || "",
+            clientSecret: process.env.GITHUB_SECRET || "",
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.SECRET,
+};
