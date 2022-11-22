429afc86acfee87b99c05779e8236495df55d29d
Tabea Schneider <abeat@posteo.net>
2022-11-22 17:41:42 +0100

    correct import statements in _app.txt

--- final-project/pages/_app.tsx
+++ final-project/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
-import store from "../store/store";
+import store from "../redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
