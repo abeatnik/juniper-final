e3957ce83eee50760d31ca7ad50006996d9e3e7f
Tabea Schneider <abeat@posteo.net>
2023-02-14 15:27:09 +0100

    modify next auth options for github login

--- final-project/pages/api/auth/[...nextauth].ts
+++ final-project/pages/api/auth/[...nextauth].ts
@@ -8,11 +8,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import prisma from "../../../lib/prisma";
 
-const authHandler: NextApiHandler = (req, res) =>
-    NextAuth(req, res, authOptions);
-export default authHandler;
-
-export const authOptions = {
+export default NextAuth({
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID || "",
@@ -28,4 +24,4 @@ export const authOptions = {
     // },
     adapter: PrismaAdapter(prisma),
     secret: process.env.NEXTAUTH_SECRET,
-};
+});
